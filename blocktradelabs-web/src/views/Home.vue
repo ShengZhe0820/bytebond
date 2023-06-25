e94fa348447ab19864d3851138ba6e3661853953
= <kang5647@LAPTOP-AP9R46KT.localdomain>
2023-06-25 17:04:12 +0800

    modify structure

--- blocktradelabs-web/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-container class="text-center">
-        <h1>This is Home Page!</h1>
-    </v-container>
-</template>
-
-<script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
